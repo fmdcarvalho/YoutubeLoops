a21510d4693c9837954270b1ecd4997492830eb8
fmdcarvalho <filipe.m.carvalho@gmail.com>
2015-06-05 21:57:07 +0100

    placeholder

--- js/background.js
+++ js/background.js
@@ -1,5 +1,7 @@
 function checkForValidURL(tabId, changeInfo,tab) {
+	//debug
 	console.log(tab.url);
+	//TODO change to a suitable regex
 	if(tab.url.indexOf('https://www.youtube.com') == 0) {
 		chrome.pageAction.show(tabId);
 	}
